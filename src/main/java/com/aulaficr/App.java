f58c634edf84c43235fa5302abe647637fb38998
Jademir de Moura <jademir.moura@vinta.com.br>
2022-04-22 19:31:44 -0300

    Atividade proposta para aula do dia 22/04/022

--- src/main/java/com/aulaficr/App.java
+++ src/main/java/com/aulaficr/App.java
@@ -4,14 +4,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Pessoa pessoa1 = new Pessoa("123456879", "Desmennyellysson Jerry", "masculino", "desmeny@mail.com");
-        /* pessoa1.printDadosCadastrais(); */
-
-        Aluno a1 = new Aluno("Desmennyellysson Jerry da Silva", "ABC1234", "GOT", 7, 8, 9.5f, 4.5f);
-        a1.printSituacaoAluno();
-
-        Aluno a2 = new Aluno("newNome", "newMatricula", "newSerie", 5, 7, 4.5f, 3);
-        a2.printSituacaoAluno();
+        //TODO: 
+        // 1 - Criar um sistema de Eventos onde podemos cadastrar eventos contendo:
+        // Evento => Titulo, dia, horario, local
+        // Cada evento pode ter pessoas inscritas:
+        // Pessoa => Nome, email, telefone, cpf
+        // O sistema deve permitir o usuário confirmar ou negar a participação
+        // O sistema deve permitir listar as pessoas que confirmaram presença no evento
 
     }
 }
